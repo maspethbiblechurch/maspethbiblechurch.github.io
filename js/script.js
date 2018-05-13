60aebb722fb952ef465c36f1dec4d5ac918a6a1e
tylergaugler16 <tylergaugler16@gmail.com>
2018-05-13 15:00:20 -0400

    trying to fix mobile screen flash error

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  // })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-78732723-1', 'auto');
-  ga('send', 'pageview');
+  // ga('create', 'UA-78732723-1', 'auto');
+  // ga('send', 'pageview');
 
 var mainBlockVisible = false;
   $(document).ready(function(){
@@ -16,11 +16,17 @@ var mainBlockVisible = false;
      });
     }
     var fixmeTop  = $('div.main-navbar').offset().top;
+
     $(document).on('scroll', function(){
-       var currentScroll = $(window).scrollTop();
-      $target = $('div.main-navbar').prev();
+      var currentScroll = $(window).scrollTop();
+      if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        $target = null;
+        }
+        else{
+          $target = $('div.main-navbar').prev();
+        }
+
        if (currentScroll >= fixmeTop) {
-         console.log("below");
          $('div.main-navbar').addClass('sticky-navbar');
          $target.css('margin-bottom', '75px');
        }
