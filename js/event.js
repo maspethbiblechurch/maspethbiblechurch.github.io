aeb0643bda172cd44ccb8fb3fbfb27665a8330b5
tylergaugler16 <tylergaugler16@gmail.com>
2018-08-01 13:48:17 -0400

    added random background colors to events without display images

---
+++ js/event.js
@@ -0,0 +1,14 @@
+  $(document).ready(function(){
+
+
+    $('.random-color').each(function(index, element){
+
+      var randomNumber = Math.floor((Math.random() * 4) + 1);
+
+      var title = $(this).attr('title')
+
+      var randomNumber = (title.charCodeAt(0) % 4) +1;
+      $(this).addClass("random-color-"+randomNumber);
+
+    });
+  });
